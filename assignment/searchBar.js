1b1e31df73f2a1bfc89fa7fc4415542471d033d7
Oguess <oguessyap@gmail.com>
2024-09-20 18:09:37 +0800

    update placement of pc for each category
    add on search bar

---
+++ assignment/searchBar.js
@@ -0,0 +1,12 @@
+document.querySelector('.laptopDatabase').addEventListener('click', () => {
+    const searchBar = document.getElementById('searchBar');
+    searchBar.style.display = 'block';
+});
+
+document.querySelector('.userDatabase').addEventListener('click', () => {
+    const searchBar = document.getElementById('searchBar');
+    searchBar.style.display = 'block';
+});
+
+
+
