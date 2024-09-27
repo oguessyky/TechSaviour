2a0230e32068a079f8568afdce1713a0f8e2375d
Oguess <oguessyap@gmail.com>
2024-09-27 13:56:52 +0800

    add js for circle process

---
+++ assignment/js/circleProgress.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const charts = document.querySelectorAll('.chart');
+    charts.forEach(chart => {
+        const percent = chart.getAttribute('data-percent');
+        let currentPercent = 0;
+        const interval = setInterval(() => {
+            if (currentPercent < percent) {
+                currentPercent++;
+                chart.style.setProperty('--percent', currentPercent);
+                chart.querySelector('span').textContent = `${currentPercent}%`;
+            } else {
+                clearInterval(interval);
+            }
+        }, 20);
+    });
+});
