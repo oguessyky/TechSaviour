4ed844c523a52b4ce547aaa7f52e4741dca1dad7
JosephIsNoob <unidentifiedperson117@gmail.com>
2024-10-03 12:14:57 +0800

    beh song "userlist is not a word" so i changed it to userList

--- assignment/sub-page/Login&Register.php
+++ assignment/sub-page/Login&Register.php
@@ -3,7 +3,7 @@
     include "Login&Register.html";
     $userQuery = $dbConn -> query("SELECT Username,Password FROM User");
     $userList = $userQuery -> fetch_all();
-    echo "<script> var userlist = ".json_encode($userList)."; </script>";
+    echo "<script> var userList = ".json_encode($userList)."; </script>";
 ?>
 <script>
 loginForm.action = "Login.php";
