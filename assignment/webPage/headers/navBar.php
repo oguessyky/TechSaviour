1130d592d4dde9437a22c0d5b49be9f28cdf4be3
Oguess <oguessyap@gmail.com>
2024-10-07 11:37:43 +0800

    while login dropdown show

--- assignment/webPage/headers/navBar.php
+++ assignment/webPage/headers/navBar.php
@@ -7,4 +7,5 @@
     include "navBar.html";
     if ($isSet) {
         echo "<script>document.getElementById('login').innerHTML = '$username'</script>";
+        echo "<style>.profile:hover .profileOptions {display: block;}</style>";
     }
