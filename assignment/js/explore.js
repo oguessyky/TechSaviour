23bb1d365409641f63e609ca78246cf875ddd06e
Oguess <oguessyap@gmail.com>
2024-10-07 16:22:24 +0800

    explore update (round button) still changing

---
+++ assignment/js/explore.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.circle-button-explore');
+    buttons.forEach(button => {
+        const initialValue = parseInt(button.getAttribute('data-initial'));
+        const maxValue = parseInt(button.getAttribute('data-max'));
+        const color = button.getAttribute('data-color');
+        button.style.setProperty('--color', color);
+        button.style.setProperty('--percent', (initialValue / maxValue) * 100);
+        button.querySelector('.number-explore').textContent = initialValue;
+    });
+});
+
+function buttonClick(element) {
+    let numberElement = element.querySelector('.number-explore');
+    let initialValue = parseInt(element.getAttribute('data-initial'));
+    let maxValue = parseInt(element.getAttribute('data-max'));
+    let currentValue = parseInt(numberElement.textContent);
+    currentValue = (currentValue + 1) % (maxValue + 1);
+    numberElement.textContent = currentValue;
+    element.style.setProperty('--percent', (currentValue / maxValue) * 100);
+}
