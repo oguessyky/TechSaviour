efcbb092d70bb225b3a2368fbc2b362f1a5b2f26
JosephIsNoob <unidentifiedperson117@gmail.com>
2024-10-08 17:25:45 +0800

    delete data

--- assignment/webPage/admin/delete.php
+++ assignment/webPage/admin/delete.php
@@ -1,3 +1,24 @@
 <?php
-    echo $_POST['id'];
-    echo $_POST['data'];
+    $header = "location: ./";
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $data = $_POST['data'];
+        $id = $_POST['id'];
+        switch ($data) {
+            case 'laptop':
+                $sql = "DELETE FROM Laptop WHERE ID = '$id'";
+                break;
+            case 'user':
+                $sql = "DELETE FROM User WHERE Username = '$id'";
+                break;
+            case 'feedback':
+                $sql = "DELETE FROM Feedback WHERE ID = '$id'";
+                break;
+        }
+        $header .= "?data=$data";
+        require "../headers/dbConn.php";
+        if (!$dbConn -> query($sql)) {
+            die("Error Deleting");
+        }
+    }
+    header($header);
+    die();
