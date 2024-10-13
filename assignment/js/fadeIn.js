70ed6b252e29b921c126cc45238d985a7ad644af
jimhanboi <songjoehan@gmail.com>
2024-10-13 23:36:23 +0800

    update

--- assignment/js/fadeIn.js
+++ assignment/js/fadeIn.js
@@ -1,18 +1,17 @@
 window.addEventListener("scroll", fadeIn);
 
-        function fadeIn(){
-            var fadeIn = document.querySelectorAll(".fadeIn");
+function fadeIn(){
+    var fadeIn = document.querySelectorAll(".fadeIn");
 
-            for(var i = 0; i<fadeIn.length; i++){
-                var windowheight = window.innerHeight;
-                var fadeInTop = fadeIn[i].getBoundingClientRect().top;
-                var fadeInPoint = 150;
+    for(var i = 0; i<fadeIn.length; i++){
+        var windowheight = window.innerHeight;
+        var fadeInTop = fadeIn[i].getBoundingClientRect().top;
+        var fadeInPoint = 150;
 
-                if(fadeInTop < windowheight - fadeInPoint){
-                    fadeIn[i].classList.add("active");
-                }
-                else{
-                    fadeIn[i].classList.remove("active");
-                }
-            }
+        if(fadeInTop < windowheight - fadeInPoint){
+            fadeIn[i].classList.add("active");
+        }else{
+            fadeIn[i].classList.remove("active");
         }
+    }
+}
